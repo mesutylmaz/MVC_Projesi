5cc18378c21a96588d5ddf01ead37052d24ffe52
Mesut Yılmaz <mesutylmaz492@gmail.com>
2022-02-06 20:30:03 +0300

    Migration İşlemi Yapılıp Proje Veritabanına Bağlandı.

---
+++ DataAccessLayer/Migrations/202202061453137_ikinci.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ikinci : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Contents", "Category_CategoryID", "dbo.Categories");
+            DropIndex("dbo.Contents", new[] { "Category_CategoryID" });
+            DropColumn("dbo.Contents", "Category_CategoryID");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Contents", "Category_CategoryID", c => c.Int());
+            CreateIndex("dbo.Contents", "Category_CategoryID");
+            AddForeignKey("dbo.Contents", "Category_CategoryID", "dbo.Categories", "CategoryID");
+        }
+    }
+}
--- EntityLayer/Concreate/Content.cs
+++ EntityLayer/Concreate/Content.cs
@@ -23,15 +23,16 @@ namespace EntityLayer.Concreate
 
 
 
-
         public int HeadingID { get; set; }     //Heading class'ından bilgi almak için bu Foreign Key propery'sini kullanacak.
-        public virtual Category Category { get; set; }      //1 Content(İçerik)'in 1 Heading(Başlık)'i olabilir. => 1
+        public virtual Heading Heading { get; set; }      //1 Content(İçerik)'in 1 Heading(Başlık)'i olabilir. => 1
                                                             //1 Heading(Başlık)'in 1'den fazla Content(İçerik)'i olabilir. => Çok
                                                             //Bu sebeple 2 class arasındaki ilişki 1'e çoktur.
 
 
 
-        public int WriterID { get; set; }     //Writer class'ından bilgi almak için bu Foreign Key propery'sini kullanacak.
+        public int? WriterID { get; set; }     //Writer class'ından bilgi almak için bu Foreign Key propery'sini kullanacak.
+                                               //2 tane Forign Key olduğundan migration'da sorun olmaması için burayı nullable(?) 
+                                               //Yani boş bırakılabilir yaptık.
         public virtual Writer Writer { get; set; }      //1 Content(İçerik)'in 1 Writer'ı olabilir. => 1
                                                         //1 Writer'ın 1'den fazla Content(İçerik)'i olabilir. => Çok
                                                         //Bu sebeple 2 class arasındaki ilişki 1'e çoktur.
