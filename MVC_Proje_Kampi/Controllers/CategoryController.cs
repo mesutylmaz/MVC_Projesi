e51be4ed1c38e27d50e92f694c881955c75b91ca
Mesut Yılmaz <mesutylmaz492@gmail.com>
2022-02-11 08:26:09 +0300

    CategoryController'ın Oluşturulması ve Index ile CategoryList View'larının Düzenlenmesi

---
+++ MVC_Proje_Kampi/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concreate;      //CategoryManager için
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Proje_Kampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        CategoryManager categoryManager = new CategoryManager();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAllBusinessLayer();
+            return View(categoryValues);
+        }
+    }
+}
